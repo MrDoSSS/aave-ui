6c571252c0a13eb456a26bc428389e493f0dece3
Artur Korotenko <artfut10@gmail.com>
2025-10-28 00:50:18 +0300

    fix: build

--- src/libs/hooks/use-get-ens-name.tsx
+++ src/libs/hooks/use-get-ens-name.tsx
@@ -1,28 +1,7 @@
-import { useState, useEffect } from 'react';
-import { ChainId } from '@aave/contract-helpers';
-import { getProvider } from '../../helpers/config/markets-and-network-config';
-
-// const mainnetProvider = getProvider(ChainId.mainnet);
+import { useState } from 'react';
 
 const useGetEnsName = (address: string) => {
-  const [ensName, setEnsName] = useState<string | null>(null);
-
-  // const getRecord = async (address: string) => {
-  //   try {
-  //     const name = await mainnetProvider.lookupAddress(address);
-  //     setEnsName(name);
-  //   } catch (error) {
-  //     console.error('ENS lookup error', error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (address) {
-  //     getRecord(address);
-  //   } else {
-  //     setEnsName(null);
-  //   }
-  // }, [address]);
+  const [ensName] = useState<string | null>(null);
 
   return { ensName };
 };
